610bb813315a5cfe7d645162eaf77b96576fc411
Vikram T <vikram.t@campk12.com>
2023-04-22 19:54:39 +0530

    DB connection setup

---
+++ config/constant.js
@@ -0,0 +1,14 @@
+const constants = {
+    DATABASE: {
+        PG: {
+            DBNAME: "dyqmvsxe",
+            PORT: 5432,
+            HOST: "john.db.elephantsql.com",
+            PASSWORD: "moS5IlJ_nJlW8BoAvEX3h5OvyvaDGUo2",
+            USERNAME: "dyqmvsxe",
+            CONNECTION_LIMIT: 20,
+        }
+    }
+}
+
+module.exports = constants;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./src/app");
+require("./core/database");
 
 app.listen(8000, () => {
     console.log("Server Started on server 8000");
